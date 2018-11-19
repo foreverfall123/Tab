49ec39ed92e233e84a6c21abb6409b306854c269
foreverfall <foreverfall@naver.com>
2018-11-19 21:14:59 +0900

    change activity_main_4

---
+++ app/src/main/java/com/namseoul/sa/tab/MembershipActivity.java
@@ -0,0 +1,17 @@
+package com.namseoul.sa.tab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by forev on 2018-11-19.
+ */
+
+public class MembershipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.membership);
+    }
+}
